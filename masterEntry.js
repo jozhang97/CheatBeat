2b71522a86edb3b510d5cbf9880f1365c5085230
Rohan <rrnarayan1@gmail.com>
2015-12-25 19:57:54 -0800

    Changed to Table
    Should make it easier to delete/modify entry

--- masterEntry.js
+++ masterEntry.js
@@ -1,16 +1,25 @@
 var answers = [];
 var main = function () {
+
     $(".submit").click( function(){
         console.log(answers);
     });
+
     $(".currAns").keypress( function(event) {
         var answer = String.fromCharCode(event.which);
-        var html="<p>"+answer+"</p>";
         answers.push(answer);
         var str=String(answers.length)+" Questions Added so far";
         $("#questionCounter").text(str);
-        $(html).appendTo("#answersCont");
+        var html="<tr class="+(answers.length-1)+"><td>"+answer+"</td><tr>";
+        $(".submittedAns").append(html)
         $(this).val("");
+    });
+
+    $(".clearButton").click( function(){
+        answers=[]
+        $(".submittedAns").text(String(answers));
+        var str=String(answers.length)+" Questions Added so far";
+        $("#questionCounter").text(str);
     });
 }
 $(document).ready(main);
