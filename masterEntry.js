8f0d5de972df47649c102d6d95c809420de08937
Rohan <rrnarayan1@gmail.com>
2015-12-28 21:40:50 -0800

    Changing answers functionality

--- masterEntry.js
+++ masterEntry.js
@@ -24,7 +24,8 @@ var main = function () {
         answers.push(answer);
         var str=String(answers.length)+" Questions Added so far";
         $("#questionCounter").text(str);
-        var html="<tr><td><a href='#'>"+answer+"</a></td><tr>";
+        var idname='row'+(answers.length-1)
+        var html="<tr id="+idname+"><td><a href='#'>"+answer+"</a></td><tr>";
         $(".submittedAns").append(html)
         $(this).val("");
     });
@@ -36,4 +37,14 @@ var main = function () {
         $("#questionCounter").text(str);
     });
 }
-$(document).ready(main).on('click','#thetable tr',function() {console.log($(this).index()/2)});
+var change= function(){
+    var rowNumber=$(this).index()/2;
+    var idname="#row"+rowNumber;
+    var newAns=window.prompt("What do you want to change it to?","Ex. A");
+    answers[rowNumber]=newAns;
+    $(idname).html("<a href='#'>"+newAns+"</a>");
+    console.log(answers);
+}
+
+$(document).ready(main).on('click','#thetable tr', change);
+
